4f0c4a7a97d065cddfbf4d10fc4e60d3cea8b794
shrav-p24 <112315179@cse.iiitp.ac.in>
2025-03-19 15:36:41 +0530

    Update Wordle.java

--- Wordle.java
+++ Wordle.java
@@ -13,11 +13,19 @@ public class Wordle{
     static void play(String random_word){
         Scanner sc = new Scanner(System.in);
         String guess = "";
-        while (!guess.equals(random_word)){
+        int counter=1;
+        while(!guess.equals(random_word) && counter<=6){
+            System.out.println("Guess #"+counter);
             guess = sc.nextLine();
             System.out.println(feedback(guess, random_word));
+            counter++;
         }
-         System.out.println("Congratulations! You guessed the correct word.");
+        if(guess.equals(random_word)){
+        System.out.println("Congratulations! You guessed the correct word.");
+        }
+        else{
+            System.out.println("Better luck next time! The correct word was: "+random_word);
+        }
         sc.close();
     }
     static String feedback(String guess, String word){
